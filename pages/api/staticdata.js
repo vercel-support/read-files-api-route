ea364224a172fa41a100d859e3c9734d7653476c
Ismael Rumzan <ismael.rumzan@gmail.com>
2022-04-18 11:09:53 +0300

    added comments

--- pages/api/staticdata.js
+++ pages/api/staticdata.js
@@ -2,7 +2,10 @@ import path from "path";
 import { promises as fs } from "fs";
 
 export default async function handler(req, res) {
-  const postsDirectory = path.join(process.cwd(), "json");
-  const fileContents = await fs.readFile(postsDirectory + "/data.json", "utf8");
+  //Find the absolute path of the json directory
+  const jsonDirectory = path.join(process.cwd(), "json");
+  //Read the json data file data.json
+  const fileContents = await fs.readFile(jsonDirectory + "/data.json", "utf8");
+  //Return the content of the data file in json format
   res.status(200).json(fileContents);
 }
